7d5e71b4a8766b7ee2549be79a393e0081c4ad08
fearlessreality <pol1997@abv.bg>
2023-03-01 22:50:39 +0200

    wrote my app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+const members = ["Chris", "Polly", "Reena The Cat Monster"];
 function App() {
+  const [name, setName] = useState("");
+  function onFormSubmit(event) {
+    event.preventDefault();
+    setName(event.target.username.value.trim());
+  }
+
+  useEffect(() => {
+    console.log(name);
+    // if (members.includes(name)) {
+    //   alert(`Welcome to the treehouse, ${name}!`);
+    // } else {
+    //   alert(`You're not cool enough!`);
+    // }
+  }, [name]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onFormSubmit}>
+        <label>
+          Enter your name:
+          <input name="username" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {name && members.includes(name) && (
+        <div>
+          <h1>Welcome to the treehouse, {name}!</h1>
+          <p>Our members are:</p>
+          <ul>
+            {members.map((member) => (
+              <li>{member}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {name && !members.includes(name) && (
+        <h1>Go away, {name}, you're drunk!</h1>
+      )}
     </div>
   );
 }
